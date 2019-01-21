03bd5626b78ce58c73ccded36f84c6f94f1d8b9b
Paul Everitt <pauleveritt@me.com>
2019-01-21 09:05:36 -0500

    Add in the steps from the "Adding Markdown Files" docs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content`,
+        name: 'markdown-pages',
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
