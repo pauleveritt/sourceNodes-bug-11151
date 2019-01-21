141359b1ec13f5f23d5a3c62622d8116d477f0d1
Paul Everitt <pauleveritt@me.com>
2019-01-21 10:32:35 -0500

    Removed all unnecessary plugins, components, etc.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
     <h1>Home Page</h1>
     <Link to="/hello/">Go to a blog post</Link>
   </Layout>
